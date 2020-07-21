ed25c3729a18e726b5558a689d0de9ecec07d52c
unknown <tommyconner96@gmail.com>
2020-07-21 19:01:28 -0400

    fully functional frontend with deployed server

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,46 @@
 import React from 'react'
+import '../styles.css'
+import { Switch, Route, Link } from 'react-router-dom'
+import Logout from './Logout'
+import Register from './Register'
+import Login from './Login'
+import Coffee from './Coffee'
+import PrivateRoute from '../utils/PrivateRoute'
 
 
 function App() {
   return (
-    <div className="App">
-      helo
+    <div className='container'>
+      <div className='nav'>
+        <Link to='/'>Home</Link>
+        <Link to='/coffee'>Coffee</Link>
+        <Link to='/login'>Login</Link>
+        <Link to='/register'>Register</Link>
+        <Link to='/logout'>Logout</Link>
+      </div>
+
+      <Switch>
+        <Route path='/logout'>
+          <Logout />
+        </Route>
+        <Route path='/register'>
+          <Register />
+        </Route>
+        <Route path='/login'>
+          <Login />
+        </Route>
+        <PrivateRoute exact path='/coffee' component={Coffee} />
+        <Route path='/'>
+          <h1>helo wrld</h1>
+          <p>This app is an example of using React and axios to interact with data on
+          restricted routes in Node that are using JWTs and cookies. Look through the
+            <br />
+            <a href='https://github.com/tommyconner96/starter-bw-frontend'>Frontend repo</a>
+            <br />
+            <a href='https://github.com/tommyconner96/starter-bw'>Backend reop</a>
+          </p>
+        </Route>
+      </Switch>
     </div>
   )
 }
---
+++ src/components/Logout.js
@@ -0,0 +1,14 @@
+import { useEffect } from 'react'
+import { useHistory } from 'react-router-dom'
+import axios from 'axios'
+
+export default function () {
+    const history = useHistory()
+
+    useEffect(() => {
+        axios.get('https://starter-bw.herokuapp.com/auth/logout', { withCredentials: true })
+            .catch((err) => console.error(err))
+            .finally(() => history.push('/'))
+    }, [history])
+    return null
+}
