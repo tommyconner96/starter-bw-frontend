3d0259ea0d7b5f279570536d6a7a694762813d58
unknown <tommyconner96@gmail.com>
2020-08-01 19:00:45 -0400

    installed recoil

--- src/utils/AxiosWithAuth.js
+++ src/utils/AxiosWithAuth.js
@@ -3,7 +3,8 @@ import axios from 'axios'
 // Next we make an 'instance' of it
 const instance = axios.create({
     // .. where we make our configurations
-    baseURL: 'https://starter-bw.herokuapp.com'
+    // baseURL: 'https://starter-bw.herokuapp.com'
+    baseURL: 'http://localhost:5000'
 })
 instance.defaults.withCredentials = true
 
